6e8a160cbe3632e9723dfb3233993bf368e598fb
jdighton <jd.qz3f2@gmail.com>
2022-09-30 12:18:12 -0500

    added shader program validation header

---
+++ sb7code/build/CMakeFiles/buffers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/buffers"
+  "../bin/buffers.pdb"
+  "CMakeFiles/buffers.dir/src/buffers/buffers.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/buffers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
